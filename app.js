6f15aec5013edaf7757c1d189010150c7da729f2
kalyanavasanth <kalyanvasanth92@outlook.com>
2017-06-21 20:49:37 +0530

    port number fix for heroku

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.get('*', function(req, res){
 	res.send('are you kidding?');
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
 	console.log('Listening to port 3000 !!!');
 });
